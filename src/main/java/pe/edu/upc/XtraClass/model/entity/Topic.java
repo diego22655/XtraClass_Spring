326920b4b0b0a95eaceee4dc3d8886a8d12f81e8
AleAguirre2 <67663678+AleAguirre2@users.noreply.github.com>
2021-06-05 01:19:48 -0500

    Update Topic.java

--- src/main/java/pe/edu/upc/XtraClass/model/entity/Topic.java
+++ src/main/java/pe/edu/upc/XtraClass/model/entity/Topic.java
@@ -1,35 +1,59 @@
 package pe.edu.upc.XtraClass.model.entity;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "topics")
-public class Topic {
-
+@Table(name = "students")
+public class Student {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(name = "namTopic", length = 15, nullable = false)
-	private String namTopic;
+	@ManyToOne
+	@JoinColumn(name="card_id")
+	private Card card;
 	
-	@Column(name = "description", length = 120)
-	private String description;
+	@ManyToOne
+	@JoinColumn(name = "person_id")
+   	private Person person;
 	
-	@OneToMany(mappedBy = "topic", fetch = Fetch )
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
 	private List<Incidence> incidences;
 	
-	public Topic() {
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
+    	private List<Forum> forums;
+	
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
+	private List<ClassReservation> classReservation;
+	
+	public Student() {
 		incidences = new ArrayList<Incidence>();
+		forums = new ArrayList<Forum>();
+		classReservation = new ArrayList<ClassReservation>();
 	}
+	
+	
+	public Card getCard() {
+		return card;
+	}
+
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
 
 	public Integer getId() {
 		return id;
@@ -39,21 +63,69 @@ public class Topic {
 		this.id = id;
 	}
 
-	public String getNamTopic() {
-		return namTopic;
+	public Person getPerson() {
+		return person;
 	}
 
-	public void setNamTopic(String namTopic) {
-		this.namTopic = namTopic;
+	public void setPerson(Person person) {
+		this.person = person;
 	}
 
-	public String getDescription() {
-		return description;
+	public List<Incidence> getIncidences() {
+		return incidences;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setIncidences(List<Incidence> incidences) {
+		this.incidences = incidences;
+	}
+
+	public List<Forum> getForums() {
+		return forums;
+	}
+
+	public void setForums(List<Forum> forums) {
+		this.forums = forums;
+	}
+
+	public List<ClassReservation> getClassReservation() {
+		return classReservation;
+	}
+
+	public void setClassReservation(List<ClassReservation> classReservation) {
+		this.classReservation = classReservation;
 	}
 	
+	public void setTarjeta(Card card) {
+		this.card = card;
+	}
+
+	public List<Incidence> getIncidencias() {
+		return incidences;
+	}
+
+	public void setIncidencias(List<Incidence> incidencias) {
+		this.incidences = incidencias;
+	}
+
+	public List<Forum> getForos() {
+		return forums;
+	}
+
+	public void setForos(List<Forum> foros) {
+		this.forums = foros;
+	}
+
+	public List<ClassReservation> getReservaClase() {
+		return classReservation;
+	}
+
+	public void setReservaClase(List<ClassReservation> reservaClase) {
+		this.classReservation = reservaClase;
+	}
+
 	
+	
+
 }
+	
+
