4701ab5c4efe5fee89277db8b071948322c2ca94
kermit031199 <83742107+kermit031199@users.noreply.github.com>
2021-06-04 22:52:27 -0500

    v1

---
+++ src/main/java/pe/edu/upc/XtraClass/service/ClassificactionConsultingService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service;
+
+import pe.edu.upc.XtraClass.model.entity.ClassificationConsulting;
+
+public interface ClassificactionConsultingService extends CrudService<ClassificationConsulting,String>{
+
+}
---
+++ src/main/java/pe/edu/upc/XtraClass/service/TeacherService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service;
+
+import pe.edu.upc.XtraClass.model.entity.Teacher;
+
+public interface TeacherService extends CrudService<Teacher,String>{
+
+}
