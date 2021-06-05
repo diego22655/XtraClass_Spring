5ec8caeb604f95bc67738e422a7abc509d49f9df
mark13571 <67776655+mark13571@users.noreply.github.com>
2021-06-05 00:24:43 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/XtraClass/service/impl/PersonServiceImpl.java
@@ -0,0 +1,21 @@
+package pe.edu.upc.XtraClass.service.crud.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import pe.edu.upc.XtraClass.model.entity.Person;
+import pe.edu.upc.XtraClass.model.repository.PersonRepository;
+import pe.edu.upc.XtraClass.service.crud.PersonService;
+
+@Service
+public class PersonServiceImpl implements PersonService{
+
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@Override
+	public JpaRepository<Person, Integer> getRepository(){
+		return personRepository;
+	}
+}
