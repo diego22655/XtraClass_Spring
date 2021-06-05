5c15c8df10b049dfbdd0a8a2cc7a8e36269a95a9
diego22655 <50973273+diego22655@users.noreply.github.com>
2021-06-05 00:32:30 -0500

    Update ForumService.java

--- src/main/java/pe/edu/upc/XtraClass/service/ForumService.java
+++ src/main/java/pe/edu/upc/XtraClass/service/ForumService.java
@@ -3,5 +3,6 @@ package pe.edu.upc.XtraClass.service;
 import pe.edu.upc.XtraClass.model.entity.Forum;
 
 public interface ForumService extends CrudService<Forum, Integer>{
-
+    List<Forum> findByIssue(String issue) throws Exception;
+	  List<Forum> findByIssueStartingWith(String issue) throws Exception;
 }
