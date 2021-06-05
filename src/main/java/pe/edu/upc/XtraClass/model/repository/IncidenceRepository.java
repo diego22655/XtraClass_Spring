8c3f5ff7ef8094c1081c9adec69a0b8ba77fec14
mark13571 <67776655+mark13571@users.noreply.github.com>
2021-06-05 00:15:21 -0500

    First commit

--- src/main/java/pe/edu/upc/XtraClass/model/repository/IncidenceRepository.java
+++ src/main/java/pe/edu/upc/XtraClass/model/repository/IncidenceRepository.java
@@ -1,9 +1,11 @@
 package pe.edu.upc.XtraClass.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.XtraClass.model.entity.Incidence;
 
+@Repository
 public interface IncidenceRepository extends JpaRepository<Incidence, Integer>{
 
 }
