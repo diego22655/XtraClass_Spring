9f77a30f3b64d94f6d7ba3f59a507af669c960fc
mark13571 <67776655+mark13571@users.noreply.github.com>
2021-06-04 22:53:08 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/XtraClass/model/entity/Forum.java
@@ -0,0 +1,62 @@
+package pe.edu.upc.XtraClass.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Forum")
+public class Forum { 
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 @Column(name="Forum_ID", columnDefinition="NUMERIC(6)")
+	 private Integer forumId;
+
+	 @Column(name="Issue", length = 25)
+	 private String issue;
+		 
+	 @Column(name="Comment", length = 255)
+	 private String comment;
+	 
+	 @ManyToOne
+	 @JoinColumn(name = "Student_ID", nullable = false)
+	 private Student student;
+
+	public Integer getForumId() {
+		return forumId;
+	}
+
+	public void setForumId(Integer forumId) {
+		this.forumId = forumId;
+	}
+
+	public String getIssue() {
+		return issue;
+	}
+
+	public void setIssue(String issue) {
+		this.issue = issue;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	 
+	 
