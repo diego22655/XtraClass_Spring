495ae1c531f7b4299310e5ee3d47bd3fb9ab5639
mark13571 <67776655+mark13571@users.noreply.github.com>
2021-06-05 00:23:05 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/XtraClass/model/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.XtraClass.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.XtraClass.model.entity.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer>{
+
+}
