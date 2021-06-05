06496be198cef5227bacd154a116466555072544
milokill <62723497+milokill@users.noreply.github.com>
2021-06-04 23:39:54 -0500

    v1

---
+++ src/main/java/pe/edu/upc/XtraClass/model/repository/PayRepository.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.XtraClass.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.XtraClass.model.entity.Pay;
+
+@Repository
+public interface PayRepository  extends JpaRepository<Pay, String>{
+
+}
---
+++ src/main/java/pe/edu/upc/XtraClass/model/repository/ProfileRepository.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.XtraClass.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.XtraClass.model.entity.Profile;
+
+@Repository
+public interface ProfileRepository  extends JpaRepository<Profile, String>{
+
+}
