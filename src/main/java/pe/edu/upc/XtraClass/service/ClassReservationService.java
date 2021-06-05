7b25c688f9a8c6080347b9bc0ba8923a8081e844
kermit031199 <83742107+kermit031199@users.noreply.github.com>
2021-06-04 23:38:40 -0500

    v1

---
+++ src/main/java/pe/edu/upc/XtraClass/service/ClassReservationService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service;
+
+import pe.edu.upc.XtraClass.model.entity.ClassReservation;
+
+public interface ClassReservationService extends CrudService<ClassReservation,Integer>{
+
+}
