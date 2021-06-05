54883e0afa090bf6ead6447b39a0f503e7186ab1
mark13571 <67776655+mark13571@users.noreply.github.com>
2021-06-04 22:58:55 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/XtraClass/service/ForumService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service.crud;
+
+import pe.edu.upc.XtraClass.model.entity.Forum;
+
+public interface ForumService extends CrudService<Forum, Integer>{
+
+}
---
+++ src/main/java/pe/edu/upc/XtraClass/service/IncidenceService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service.crud;
+
+import pe.edu.upc.XtraClass.model.entity.Incidence;
+
+public interface IncidenceService extends CrudService<Incidence,Integer>{
+
+}
