3ad5ce349f6f2fbd8867bf2087c4f9dfe61fabc5
milokill <62723497+milokill@users.noreply.github.com>
2021-06-04 23:40:31 -0500

    v1

---
+++ src/main/java/pe/edu/upc/XtraClass/service/PayService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service;
+
+import pe.edu.upc.XtraClass.model.entity.Pay;
+
+public interface PayService extends CrudService<Pay,String>{
+
+}
---
+++ src/main/java/pe/edu/upc/XtraClass/service/ProfileService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.XtraClass.service;
+
+import pe.edu.upc.XtraClass.model.entity.Profile;
+
+public interface ProfileService extends CrudService<Profile,String>{
+
+}
