616479594734a0e6a6c4b91e860b13ca83a59cd6
kermit031199 <83742107+kermit031199@users.noreply.github.com>
2021-06-04 22:51:07 -0500

    v1

---
+++ src/main/java/pe/edu/upc/XtraClass/model/entity/ClassificationConsulting.java
@@ -0,0 +1,58 @@
+package pe.edu.upc.XtraClass.model.entity;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "Classification_Consulting")
+public class ClassificationConsulting {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Classification_ID")
+	private Integer id;
+
+	@Column(name = "last_name", length = 10)
+	private String NameClassification;
+	
+	//@OneToMany(mappedBy = "calificacion", fetch = FetchType.LAZY)
+	//private List<ReservaClase> reservacion;
+	
+	// -- Constructor, Getter, Setter
+	public ClassificationConsulting() {
+		//reservacion = new ArrayList<ReservaClase>();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNameClassification() {
+		return NameClassification;
+	}
+
+	public void setNameClassification(String NameClassification) {
+		this.NameClassification = NameClassification;
+	}
+
+	/*public List<ReservaClase> getReservacion() {
+		return reservacion;
+	}
+
+	public void setReservacion(List<ReservaClase> reservacion) {
+		this.reservacion = reservacion;
+	}
+	*/
+
+}
---
+++ src/main/java/pe/edu/upc/XtraClass/model/entity/Teacher.java
@@ -0,0 +1,89 @@
+package pe.edu.upc.XtraClass.model.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/*import pe.edu.upc.pandemia.models.entities.MateriaProfesor;
+import pe.edu.upc.pandemia.models.entities.Persona;
+import pe.edu.upc.pandemia.models.entities.ReservaClase;*/
+
+@Entity
+@Table (name = "Teacher")
+public class Teacher {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "AccountNumber", length = 20, nullable = false)
+    private String AccountNumber;
+    
+    @ManyToOne
+	@JoinColumn(name = "person_id")
+	private Person person;
+    
+    /*@OneToMany(mappedBy = "teacher", fetch = FetchType.LAZY)
+    private List<ReservaClase> reservaciones;
+
+    @OneToMany(mappedBy = "teacher", fetch = FetchType.LAZY)
+    private List<SubjectTeacher> subjectTeacher;*/
+    
+    // -- Constructor, Getter, Setter
+    public Teacher() {
+    	reservaciones = new ArrayList<ReservaClase>();
+    	subjectTeacher = new ArrayList<SubjectTeacher>();
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAccountNumber() {
+		return AccountNumber;
+	}
+
+	public void setAccountNumber(String AccountNumber) {
+		this.AccountNumber = AccountNumber;
+	}
+
+	public Person getPersona() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	/*public List<ReservaClase> getReservaciones() {
+		return reservaciones;
+	}
+
+	public void setReservaciones(List<ReservaClase> reservaciones) {
+		this.reservaciones = reservaciones;*/
+	}
+
+	public List<SubjectTeacher> getSubjectTeacher() {
+		return subjectTeacher;
+	}
+
+	public void setSubjectTeacher(List<SubjectTeacher> subjectTeacher) {
+		this.subjectTeacherr = subjectTeacher;
+	}
+
+
+}
