485e08374a78eff894c838a22a05cf41e3829bbd
AleAguirre2 <67663678+AleAguirre2@users.noreply.github.com>
2021-06-04 22:54:03 -0500

    V1

---
+++ src/main/java/pe/edu/upc/XtraClass/model/entity/Student.java
@@ -0,0 +1,52 @@
+package pe.edu.upc.XtraClass.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	//@ManyToOne
+	//@JoinColumn(name = "persona_id")
+   //private Person person;
+	
+	//@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
+	//private List<Incidence> incidences;
+	
+	//@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
+    //private List<Forum> forums;
+	
+	//@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
+	//private List<ClassReservation> classReservation;
+	
+	public Student() {
+		//incidences = new ArrayList<Incidence>();
+		//forums = new ArrayList<Forum>();
+		//classReservation = new ArrayList<ClassReservation>();
+	}
+		
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/pe/edu/upc/XtraClass/model/entity/Topic.java
@@ -0,0 +1,59 @@
+package pe.edu.upc.XtraClass.model.entity;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "topics")
+public class Topic {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "namTopic", length = 15, nullable = false)
+	private String namTopic;
+	
+	@Column(name = "description", length = 120)
+	private String description;
+	
+	//@OneToMany(mappedBy = "topic", fetch = Fetch )
+	//private List<Incidence> incidences;
+	
+	public Topic() {
+		//incidences = new ArrayList<Incidence>();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNamTopic() {
+		return namTopic;
+	}
+
+	public void setNamTopic(String namTopic) {
+		this.namTopic = namTopic;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
